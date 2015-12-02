e02ed98434ce3e835372998a3d36c649fca13370
YANG Zhenfei <yangzhenfei0@gmail.com>
2015-12-02 14:20:59 +0800

    removed redundant vundo from install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ mv ~/.vimrc vimrc.bak
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir vbackup vundo vswap vundo vinfo
+mkdir vbackup vswap vundo vinfo
 cp clang-format ../.clang-format
 cp ycm_extra_conf.py ../.ycm_extra_conf.py
